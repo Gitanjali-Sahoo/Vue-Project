8a9623c29d214470f567ac00e1d466420136bb6e
Gitanjali Sahoo <gr.sahoo15@gmail.com>
2023-02-08 21:06:57 +0100

    navbar component

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Import bootstrap for css and js
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(bootstrap)
 
 app.mount('#app')
