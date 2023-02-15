025fd1da3dfa53b233bdc34d1e6c4828b6b9ab5c
Gitanjali Sahoo <gr.sahoo15@gmail.com>
2023-02-15 13:59:00 +0100

    updated about

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,19 +1,19 @@
 <script setup>
-    import Form from '../components/Form.vue'
-    import Footer from '../components/Footer.vue'
+import Form from "../components/Form.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
-    <Form />
-    <div id="footer">
-        <Footer />
-    </div>
+  <Form />
+  <div id="footer">
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
-    #footer {
-        background-color: rgb(225, 220, 197);
-        padding: 20px;
-        position: sticky;
-    }
+#footer {
+  background-color: rgb(225, 220, 197);
+  padding: 20px;
+  position: sticky;
+}
 </style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,43 +1,42 @@
 <script setup>
-    import HeroImage from '../components/HeroImage.vue'
-    import Cards from '../components/Cards.vue'
-    import Fetch from '../components/Fetch.vue'
-    // import ImageSlider from '../components/ImageSlider.vue'
-    import Footer from '../components/Footer.vue'
+import HeroImage from "../components/HeroImage.vue";
+import Cards from "../components/Cards.vue";
+import Fetch from "../components/Fetch.vue";
+// import ImageSlider from '../components/ImageSlider.vue'
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
-    <HeroImage />
+  <HeroImage />
 
-    <Cards />
-    <Fetch />
-    <!-- <div id="slider-app">
+  <Cards />
+  <Fetch />
+  <!-- <div id="slider-app">
         <ImageSlider />
     </div> -->
 
-    <div id="footer">
-        <Footer />
-    </div>
+  <div id="footer">
+    <Footer />
+  </div>
 </template>
 <script>
-    // export default {
-    // components: { ImageSlider },
+// export default {
+// components: { ImageSlider },
 
-    // }
+// }
 </script>
 
 <style scoped>
-    /* #slider-app {
+/* #slider-app {
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
 
         justify-content: center;
     } */
-    #footer {
-        background-color: rgb(225, 220, 197);
-        padding: 20px;
-        position: sticky;
-    }
-    
+#footer {
+  background-color: rgb(225, 220, 197);
+  padding: 20px;
+  position: sticky;
+}
 </style>
